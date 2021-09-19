e1d7c2f6646f70d9261220bf4e578b80826f99e2
Pradeep Kumar Sharma <76009650+CODE-Pradeep@users.noreply.github.com>
2021-09-20 00:58:38 +0530

    Update onetonprime.java

--- onetonprime.java
+++ onetonprime.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author prk
- */
 public class onetonprime {
     
     public static void main(String s[]){
